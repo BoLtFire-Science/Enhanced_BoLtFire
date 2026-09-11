65e75677cc22898b1b99dc792fc3cd2b85a24b34
BoLtFire-Science <122883324+BoLtFire-Science@users.noreply.github.com>
2026-09-11 13:09:57 +0200

    Update to MODIS NPP
    Switched from MODIS NPP download to PsnNet

--- data_download_code/Parameter_Download/MODIS_NPP.py
+++ data_download_code/Parameter_Download/MODIS_NPP.py
@@ -1,70 +1,114 @@
 import os
-from datetime import datetime
+import shutil
+import requests
+from datetime import datetime, timedelta
 
 import ee
 import geopandas as gpd
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
-from Utilities.Path_Utilities import get_enhanced_boltfire_location, get_input_parameters
+from Utilities.Path_Utilities import getEnhancedBoltFire_Location, getInputParameters
 from Utilities.Most_Used_Functions import snap_bbox_to_grid
 
+def MODIS_download_geotiff(image, bounds, geotiff_path, scale=500):
+    if os.path.exists(geotiff_path):
+        print(f"{geotiff_path} exists, skipping!")
+        return True
 
-def get_modis_download_variables():
-    """Returns a list of dictionaries containing the necessary information for downloading MODIS NPP data."""
-    npp = {
-        "image_url": "MODIS/061/MOD17A3HGF",
-        "variable": "Npp",
+    try:
+        url = image.getDownloadUrl({
+            "bands": ["PsnNet"],
+            "region": bounds.getInfo()["coordinates"],
+            "scale": scale,
+            "crs": "EPSG:4326",
+            "format": "GEO_TIFF",})
+
+        response = requests.get(url, stream=True)
+
+        if response.status_code == 200:
+            with open(geotiff_path, "wb") as f:
+                shutil.copyfileobj(response.raw, f)
+
+            print(f"Saved GeoTIFF to {geotiff_path}")
+            return True
+        else:
+            print(f"GeoTIFF download failed.")
+            return False
+
+    except Exception as e:
+        print(f"Error downloading GeoTIFF")
+        return False
+
+
+
+def MODIS_download_variables():
+    PsnNet = {
+        "image_url": "MODIS/061/MOD17A2HGF",
+        "variable": "PsnNet",
         "resolution": 500,
         "scale": 0.0001,
     }
-    return [npp], {"Npp": npp}
+    return [PsnNet], {"PsnNet": PsnNet}
 
 
-def download_modis_numpy(image, bounds, npy_output_path, scale_factor=0.0001):
-    """Downloads the specified MODIS variable as a NumPy array, applying the given scale factor, and saves it to the specified path."""
+def MODIS_download_numpy_direct(image, bounds, npy_output_path, scale_factor=0.0001, fill_value=np.nan):
     if os.path.exists(npy_output_path):
         print(f"{npy_output_path} skipping, already there")
         return True
 
-    ee_fill_value = -30001
+    try:
+        ee_fill_value = -30001
 
-    sample = image.sampleRectangle(region=bounds,defaultValue=ee_fill_value).getInfo()
+        sample = image.sampleRectangle(region=bounds,defaultValue=ee_fill_value).getInfo()
 
-    arr = np.array(sample["properties"]["Npp"], dtype=np.float32)
-    arr[arr == ee_fill_value] = np.nan
+        arr = np.array(sample["properties"]["PsnNet"], dtype=np.float32)
+        arr[arr == ee_fill_value] = np.nan
 
-    arr *= scale_factor
-    np.save(npy_output_path, arr)
+        arr *= scale_factor
+        np.save(npy_output_path, arr)
 
-    print(f"Saved NumPy array to {npy_output_path}")
-    return True
+       # print(f"Saved NumPy array to {npy_output_path}")
+        return True
+
+    except Exception as e:
+        print(f"Error saving NumPy array {npy_output_path}: {e}")
+        return False
 
 
 
-def download_modis(lightning_path, LIWtype):
-    """For each fire, downloads MODIS NPP data for the year of the fire and saves it as .npy files."""
+
+def MODIS_downloads(lightning_path, LIWtype):
     fires = gpd.read_file(lightning_path).to_crs(4326)
 
     downloaded_fires = []
     failed_fires = []
     saved_fires = 0
 
-    _, key_vars = get_modis_download_variables()
+    _, key_vars = MODIS_download_variables()
 
-    for _, row in fires.iterrows():
+    for _, row in tqdm(fires.iterrows(), total=len(fires)):
         fire_id = str(row["FireID"]).split(".")[0]
 
         if fire_id in downloaded_fires:
             continue
 
-        output_folder = os.path.join(get_enhanced_boltfire_location(), fire_id, LIWtype)
+        output_folder = os.path.join(getEnhancedBoltFire_Location(), fire_id, LIWtype)
         os.makedirs(output_folder, exist_ok=True)
 
         start_date_dt = datetime.strptime(row["Str_Time"][:10], "%Y-%m-%d")
         start_date = start_date_dt.strftime("%Y-%m-%d")
-        year = start_date_dt.strftime("%Y")
-        date_str = start_date.replace("-", "")
+   
+        #date_str = start_date.replace("-", "")
+
+        StartDate_dt = datetime.strptime(start_date, "%Y-%m-%d")
+        date_str = StartDate_dt.strftime("%Y%m%d")
+
+  ## from beginning of the year to the start of the lightning strike
+        accum_start_dt = datetime(StartDate_dt.year, 1, 1)
+        start_str = accum_start_dt.strftime("%Y-%m-%d")
+        end_str = StartDate_dt.strftime("%Y-%m-%d") 
 
 
         bounds = [
@@ -91,25 +135,34 @@ def download_modis(lightning_path, LIWtype):
         for _, var_info in key_vars.items():
             variable = var_info["variable"]
 
-            if variable != "Npp":
-                continue
+            try:
+                if variable != "PsnNet":
+                    continue
 
-            npy_path = os.path.join(output_folder, f"NPP_{date_str}.npy")
-    
-            image = (ee.ImageCollection(var_info["image_url"]).filterDate(f"{year}-01-01", f"{int(year) + 1}-01-01").first().select(variable))
+                npy_path = os.path.join(output_folder, f"Fixed_Cummulative_PsnNet_{date_str}.npy")
 
-            if image is None:
-                print(f"No image found for NPP for FireID {fire_id}")
-                downloads_worked = False
-                continue
+                collection = ee.ImageCollection(var_info["image_url"]).filterDate(f"{start_str}", f"{end_str}").select("PsnNet")
 
-            image = image.reproject( crs="EPSG:4326",   scale=var_info["resolution"]).clip(bounds)
+       
+                n_images = collection.size().getInfo()
+                if n_images == 0:
+                    print(f"No PsnNet images found for {fire_id} in window {start_str}–{end_str}")
+                    downloads_worked = False
+                    continue
 
-            npy_download = download_modis_numpy(image=image, bounds=bounds, npy_output_path=npy_path, scale_factor=var_info["scale"])
+                image = collection.sum().clip(bounds)
 
-            if npy_download:
-                print(f"NPP geotiff and numpy downloaded for {fire_id}")
-            else:
+                image = image.reproject( crs="EPSG:4326",   scale=var_info["resolution"])
+
+                npy_download = MODIS_download_numpy_direct(image=image, bounds=bounds, npy_output_path=npy_path, scale_factor=var_info["scale"])
+
+                # if  npy_download: # tiff_download and
+                #     print(f"PsnNet geotiff and numpy downloaded for {fire_id}")
+                # else:
+                #     downloads_worked = False
+
+            except Exception as e:
+                print(f" Problem downloading {fire_id}")
                 downloads_worked = False
 
         if downloads_worked:
@@ -118,10 +171,10 @@ def download_modis(lightning_path, LIWtype):
         else:
             failed_fires.append(fire_id)
 
-    print(f" NPP download completed - successful: {saved_fires}, failed: {len(failed_fires)}")
+    print(f" PsnNet download completed - successful: {saved_fires}, failed: {len(failed_fires)}")
 
     if failed_fires:
         failed_df = pd.DataFrame(failed_fires, columns=["FailedFireID"])
-        failed_export_path = os.path.join(get_input_parameters(), "NPP_Failed_Fires.csv")
+        failed_export_path = os.path.join(getInputParameters(), "PsnNet_Failed_Fires.csv")
         failed_df.to_csv(failed_export_path, index=False)
         print(f"Failed Fires exported to: {failed_export_path}")
